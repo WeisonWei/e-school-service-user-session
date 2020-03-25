570f428cb3b8fe46ed761af0d7d39b6cd54c7a7e
WeisonWei <weixx3@126.com>
2020-03-25 10:40:38 +0800

    update pom

--- src/main/java/com/es/user/controller/SessionController.java
+++ src/main/java/com/es/user/controller/SessionController.java
@@ -1,6 +1,7 @@
 package com.es.user.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +10,13 @@ import javax.servlet.http.HttpSession;
 @RestController
 public class SessionController {
 
-    @RequestMapping("/set")
+    @PostMapping("/set")
     public String setSession(HttpSession session, @RequestParam("name") String name) {
         session.setAttribute("hello", name);
         return "ok!";
     }
 
-    @RequestMapping("/get")
+    @GetMapping("/get")
     public String getSession(HttpSession session) {
         return session.getAttribute("hello").toString();
     }

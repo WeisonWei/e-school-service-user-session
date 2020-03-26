ee44a458b3fdd25d70505f6940e04b972c7be72a
WeisonWei <weixx3@126.com>
2020-03-26 19:13:08 +0800

    update

--- src/main/java/com/es/user/session/controller/SessionController.java
+++ src/main/java/com/es/user/session/controller/SessionController.java
@@ -1,8 +1,8 @@
 package com.es.user.session.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpSession;
 @RestController
 public class SessionController {
 
-    @PostMapping("/set")
-    public String setSession(HttpSession session, @RequestParam("name") String name) {
+    @PostMapping("/set/{name}")
+    public String setSession(HttpSession session, @PathVariable("name") String name) {
         session.setAttribute("hello", name);
         return "ok!";
     }
---
+++ start-mongo.sh
@@ -0,0 +1,3 @@
+#!/bin/ bash
+cd /usr/local/mongodb-macos-x86_64-4.2.5/bin
+sudo ./mongod
